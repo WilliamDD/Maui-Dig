75817b68016846235ed422b6ab4782b75dac5973
William <william@dedooij.eu>
2022-01-26 22:36:12 +0100

    Currently broken

--- Maui-Dig/MainPage.xaml.cs
+++ Maui-Dig/MainPage.xaml.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Net;
 using DnsClient;
 using DnsClient.Protocol;
+using DnsClient.Protocol.Options;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 
 namespace Maui_Dig
 {
-	public partial class MainPage : ContentPage
+	public partial class MainPage : FlyoutPage
 	{
 		public MainPage()
 		{
@@ -37,12 +38,31 @@ namespace Maui_Dig
 
 			var lookupClient = new LookupClient(clientOptions);
 			var result = await lookupClient.QueryAsync(query, queryType,queryClass);
-			var resultList = new List<Cell>();
-			foreach (var cell in result.Answers.OfType<ARecord>())
-            {
-				resultList.Add(new TextCell() { Text = cell.DomainName, Detail = $"{cell.RecordClass} {cell.RecordType} {cell.TimeToLive} {cell.Address}" });
-            }
-			resultView.ItemsSource = resultList;
+
+			resultText.Text += result.Header;
+			resultText.Text += Environment.NewLine;
+			resultText.Text += Environment.NewLine;
+
+			resultText.Text += ";; OPT PSEUDOSECTION:";
+			foreach (OptRecord additional in result.Additionals)
+			{
+				resultText.Text += $"; EDNS: version: {additional.Version}, flags:; UDP: {additional.UdpSize}; code: {additional.ResponseCodeEx}; dnsSec: {additional.IsDnsSecOk}";
+			}
+
+			resultText.Text += Environment.NewLine;
+			resultText.Text += Environment.NewLine;
+			resultText.Text += ";; Question section";
+			foreach (var question in result.Questions)
+			{
+				resultText.Text += question;
+			}
+			resultText.Text += Environment.NewLine;
+			resultText.Text += Environment.NewLine;
+			resultText.Text += ";; Answer section";
+			foreach (var answer in result.Answers)
+			{
+				resultText.Text += answer;
+			}
 
 			//SemanticScreenReader.Announce(resultView.ItemsSource);
 		}
